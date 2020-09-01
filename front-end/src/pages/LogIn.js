71f7f872aa6508cf60e454aa8499dc283d7d3d07
edu93 <mailing_93@hotmail.com>
2020-08-31 22:46:51 -0500

    Pagina de LogIn creada, y peticion axios creada y verificada, si devuelve el token

--- front-end/src/pages/LogIn.js
+++ front-end/src/pages/LogIn.js
@@ -1,10 +1,67 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AUTH_SERVICE from '../services/auth';
+
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+ wrapperCol: { offset: 8, span: 8 },
+};
+
+const tailLayout = {
+ wrapperCol: { offset: 14, span: 16 },
+};
 
 export default class LogIn extends Component {
+
+ onFinish = async (values) => {
+  const { data } = await AUTH_SERVICE.LOGIN(values)
+  const { token } = data
+  localStorage.setItem("token", token)
+  this.props.history.push("/profile")
+ };
+
  render() {
   return (
-   <div>
+   <div >
+    <Form
+     {...layout}
+     name="basic"
+     onFinish={this.onFinish}
+    >
 
+     <Form.Item
+      name="email"
+      rules={[
+       {
+        type: 'email',
+        message: 'The input is not a valid e-mail',
+       },
+       {
+        required: true,
+        message: 'Please input your e-mail!',
+       },
+      ]}
+     >
+      <Input placeholder="E-mail" />
+     </Form.Item>
+     <Form.Item
+      name="password"
+      rules={[
+       {
+        required: true,
+        message: 'Please input your password!',
+       },
+      ]}
+     >
+      <Input.Password placeholder="Password" />
+     </Form.Item>
+
+     <Form.Item {...tailLayout}>
+      <Button type="primary" htmlType="submit">
+       Submit
+                  </Button>
+     </Form.Item>
+    </Form>
    </div>
   )
  }
--- front-end/src/router.js
+++ front-end/src/router.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import SignUp from './pages/SignUp'
+import LogIn from './pages/LogIn'
 
-const Login = () => <h1>Login holi</h1>
 const Profile = () => <h1>Profile</h1>
 
 
 const AppRouter = () => (
  <Router>
   <Route exact component={SignUp} path="/signup" />
-  <Route exact component={Login} path="/login" />
+  <Route exact component={LogIn} path="/login" />
   <Route exact component={Profile} path="/profile" />
  </Router>
 )
